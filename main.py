89f2e19807458b1a80e8115c9f0c0199c51e7cfe
Nickvi <Nickvi@bk.ru>
2017-09-14 13:35:53 +0300

    need add dfs for sensors tree

--- main.py
+++ main.py
@@ -7,84 +7,40 @@ def indexes(lst: List[Any], element: int)->list:
     return [i for i, elem in enumerate(lst) if element == elem]
 
 
-def create_tree(num_of_sensors: int)->list:
-    """Функция для инициализации сенсорного дерева"""
-    sensor_list = [Sensor(i) for i in range(0, num_of_sensors)]
-    return sensor_list
-
-
-class Sensor:
-    """Сенсор для сенсорного дерева"""
-    def __init__(self, sensor_num: int):
-        self.blocked = False
-        self.sensor_num = sensor_num  # порядковый номер сенсора
-        self.need_send_msg = [sensor_num]  # хранится ли в сенсоре сообщение
-
-
-class SensorTree:
-    """Сенсорное дерево"""
-    def __init__(self, c_matrix: List[List[int]]):
-        self.adjacency_matrix = c_matrix  # Матрица смежности
-        self.num_of_sensors = len(self.adjacency_matrix)  # количество сенсоров в дереве
-        self.sensor_list = create_tree(self.num_of_sensors)  # список всех сенсоров
-        self.bs_is_not_full = True  # истинно когда все сенсоры передали сообщения
-
-    def sensors_update(self)->None:
-        """Убирает блокировку передачи"""
-        for sensor in self.sensor_list:
-            if sensor is not None:
-                sensor.blocked = False
-
 if __name__ == '__main__':
     schedules = []
-    for matrix in adjacency_matrices:
-        sensor_tree = SensorTree(matrix)
-        del matrix
-        adjacency_matrix = sensor_tree.adjacency_matrix
-        sensors = sensor_tree.sensor_list
+    for adjacency_matrix in adjacency_matrices:
+        need_send_msg = [[i] if i != 0 else 0 for i in range(len(adjacency_matrix))]
         schedule = []
         slot_num = 0
-
-        while sensor_tree.bs_is_not_full:
+        while need_send_msg[0] != len(adjacency_matrix)-1:
+            blocked_sensors = [False for _ in range(len(adjacency_matrix))]
             schedule.append([])  # создаем слот
+            for from_sensor in range(len(adjacency_matrix)):
+                # определяем сенсор получатель
+                to_sensor = (adjacency_matrix[from_sensor].index(1)
+                             if adjacency_matrix[from_sensor].index(1) != from_sensor
+                             else adjacency_matrix[from_sensor].index(1, from_sensor + 1))
 
-            for sensor in sensors:
-                sensor_num = sensor.sensor_num
-                if sensor_num > 0:  # если сенсор не БС
-
-                    # определяем сенсор получатель
-                    to_sensor = (adjacency_matrix[sensor_num].index(1)
-                                 if adjacency_matrix[sensor_num].index(1) != sensor_num
-                                 else adjacency_matrix[sensor_num].index(1, sensor_num+1))
-
-                    if (not sensors[to_sensor].blocked  # если сенсор получатель не блокирован
-                       and not sensor.blocked           # и сенсор отправитель не блокирован
-                       and sensor.need_send_msg):       # и сенсору необходимо послать сообщение:
-
-                        # определяем каким сенсорам необходимо блокировать передачу сообщений во избежания коллизий
-                        indexes_for_block = indexes(sensor_tree.adjacency_matrix[sensor_num], 1)
-                        # добавляем в расписания сенсоры отправителя и получателя
-                        schedule[slot_num].append((sensor_num, to_sensor))
-                        # добавляем в очередь сообщений сенсора получателя(если не БС)
-                        if sensors[to_sensor].sensor_num > 0:
-                            sensors[to_sensor].need_send_msg.append(sensor_num)
-                        # блокируем сенсоры
-                        for i in indexes_for_block:
-                            sensors[i].blocked = True
-                        # для сенсора отправителя убираем необходимость передачи сообщения
-                        del sensor.need_send_msg[0]
-
-                # для БС отсутствует необходимость в передаче сообщения
-                elif sensor.sensor_num == 0:
-                    sensor.need_send_msg = []
-
-            else:
-                # закрываем слот и убираем блокировки сенсоров
-                slot_num += 1
-                sensor_tree.sensors_update()
-                # если пустой слот, значит конец фрейма и расписание составлено
-                if not schedule[-1]:
-                    schedule.remove([])
-                    sensor_tree.bs_is_not_full = False
+                if (blocked_sensors[to_sensor] is not True      # если сенсор получатель не блокирован
+                   and blocked_sensors[from_sensor] is not True  # и сенсор отправитель не блокирован
+                   and need_send_msg[from_sensor]                # и сенсору необходимо послать сообщение:
+                   and from_sensor != 0):                        # и сенсор не БС
+                    # определяем каким сенсорам необходимо блокировать передачу сообщений во избежания коллизий
+                    indexes_for_block = indexes(adjacency_matrix[from_sensor], 1)
+                    # добавляем в расписание сенсоры отправителя и получателя
+                    schedule[slot_num].append((from_sensor, to_sensor))
+                    # добавляем в очередь сообщений сенсора получателя(если не БС),
+                    # если БС увеличиваем количество полученных сообщений на 1
+                    if to_sensor != 0:
+                        need_send_msg[to_sensor].append(from_sensor)
+                    else:
+                        need_send_msg[to_sensor] += 1
+                    # блокируем сенсоры
+                    for i in indexes_for_block:
+                        blocked_sensors[i] = True
+                    # удаляем сообщение из очереди сообщений сенсора отправителя
+                    del need_send_msg[from_sensor][0]
+            slot_num += 1
         schedules += [(schedule, len(schedule))]
     print(*schedules, sep='\n')
