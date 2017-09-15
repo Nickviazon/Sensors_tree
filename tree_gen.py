1db0d98066800a628e8cc6e4e184980028e0d499
Nickvi <Nickvi@bk.ru>
2017-09-16 01:28:40 +0300

    problems with backwards compatibility removed new probability calculate added

--- tree_gen.py
+++ tree_gen.py
@@ -1,25 +1,34 @@
-from typing import Any, List
-from random import choices
-from numpy import array
-import networkx as nx
+from numpy.random import choice
 
 
-def tree_generator(n: int = 1) -> List:
+def tree_generator(n):
     """Генерирует сенсорное дерево на основе количества сеноров"""
-    sensors_tree = [0, ]
+
+    def prob_recalc(tree):
+        """Перерасчитывает вероятность с кем будет связан новый сенсор"""
+        edges_from_node = []
+        for sensor in tree:
+            edges_from_node.append(adjacency_matrix[sensor].count(1) - 1)
+        edges_num = sum(edges_from_node) / 2
+        prb = [edges_num / elem for elem in edges_from_node]
+        prb = [prob / edges_num for prob in prb]
+        sum_prb = sum(prb)
+        prb = [prob / sum_prb for prob in prb]
+        return prb
+
+    sensors_tree = [0, ]  # инициализируем дерево
+    # инициализируем матрицу смежности с 1 по главной диоганали
     adjacency_matrix = [[0 if i != j else 1 for i in range(n + 1)]
                         for j in range(n + 1)]
-
     prob_for_sensors = [1 if i == 0 else 0 for i in sensors_tree]
     for i in range(1, n+1):
-        index_for_concat = choices(sensors_tree, prob_for_sensors)[0]
+        # выбираем сенсор для соединения и вносим информацию о связи в матрицу смежности
+        index_for_concat = choice(sensors_tree, p=prob_for_sensors)
         adjacency_matrix[index_for_concat][i] = 1
         adjacency_matrix[i][index_for_concat] = 1
         sensors_tree.append(i)
-        count_elem = []
-        for sensor in sensors_tree:
-            count_elem.append(adjacency_matrix[sensor].count(1) - 1)
-        prob_for_sensors = [(sum(count_elem) / 2) / elem for elem in count_elem]
+        # перерасчитываем вероятности с учетом добавленного сенсора в сеть
+        prob_for_sensors = prob_recalc(sensors_tree)
     return adjacency_matrix
 
 
