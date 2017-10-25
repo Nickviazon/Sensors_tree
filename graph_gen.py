92810789f1344149b4677ca0433229cc114d7450
Nickvi <Nickvi@bk.ru>
2017-10-25 21:59:36 +0300

    graph generator ended

--- graph_gen.py
+++ graph_gen.py
@@ -1,5 +1,5 @@
-from numpy.random import choice
-from random import randint, uniform
+from help_functions import indexes
+from random import uniform
 
 
 def graph_generator(n):
@@ -8,56 +8,71 @@ def graph_generator(n):
     :param n - number of sensors without base station
     """
     def in_circle(x0, y0, x, y, r):
+        """
+        Checks that the point with coordinates
+         (x,y) is in the circle
+        :param x0: x coordinate of center of radius
+        :param y0: y coordinate of center of radius
+        :param x: x coordinate of consider point
+        :param y: y coordinate of consider point
+        :param r: radius of circle
+        :return: True if point in radius, else False
+        """
         return ((x - x0) ** 2 + (y - y0) ** 2) <= (r ** 2)
 
-    def prob_recalc(tree):
-        """Перерасчитывает вероятность с кем будет связан новый сенсор"""
-        edges_from_node = []
-        for sensor in tree:
-            edges_from_node.append(adjacency_matrix[sensor].count(1) - 1)
+    def bfs(start_node, goal_node, adj_matrix):
+        """
+        Breadth first search
+        :param start_node:
+        :param goal_node:
+        :param adj_matrix: adjacency matrix
+        :return: True if start node has path with goal node, else False
+        """
+        queue = []
+        visited = [False for _ in range(len(adj_matrix))]
+        visited[start_node] = True
+        queue.append(start_node)
+        while queue:
+            node = queue.pop()
+            if node == goal_node:
+                return True
 
-        if not all(edges_from_node):
-            return [1 / len(tree) for _ in tree]
-
-        edges_num = sum(edges_from_node) / 2
-        prb = [edges_num/(edges_num*elem*2) if elem != 0 else 1 for elem in edges_from_node]
-
-        if prb == [1 for _ in tree]:
-            return [1/len(tree) for _ in tree]
-        else:
-            prb = [prob / edges_num for prob in prb]
-            sum_prb = sum(prb)
-            prb = [prob / sum_prb for prob in prb]
-            return prb
+            for child in indexes(adj_matrix[node], 1):
+                if visited[child] is False:
+                    queue.append(child)
+                    visited[child] = True
+        return False
 
     # инициализируем матрицу смежности с 1 по главной диоганали
-    adjacency_matrix = [[0 if i != j else 1 for i in range(n + 1)]
+    adjacency_matrix = [[0 if k != j else 1 for k in range(n + 1)]
                         for j in range(n + 1)]
+
     # определяем координаты сенсоров и радиус их действия
-    senosrs_coords = [(0, 0)] + [(uniform(-10, 10), uniform(-10, 10)) for _ in range(n)]
-    radius = 5
+    senosrs_coords = [(0.5, 0.5)] + [(uniform(0, 1), uniform(0, 1)) for _ in range(n)]
+    radius = 0.2
 
     for i in range(len(adjacency_matrix)):
+
         while True:
+
             x0, y0 = senosrs_coords[i]
             sens_in_circle = [j for j, (x, y) in enumerate(senosrs_coords)
                               if in_circle(x0, y0, x, y, radius) and (x, y) != (x0, y0)]
-            if len(sens_in_circle) < 2:
-                senosrs_coords[i] = (uniform(-10, 10), uniform(-10, 10))
+
+            for j in sens_in_circle:
+                adjacency_matrix[j][i] = 1
+                adjacency_matrix[i][j] = 1
+
+            path_to_bs = bfs(i, 0, adjacency_matrix)
+            # если у сенсора нет пути до бс
+            if i != 0 and not path_to_bs:
+                senosrs_coords[i] = (uniform(0, 1), uniform(0, 1))
+                for j in sens_in_circle:
+                    adjacency_matrix[j][i] = 0
+                    adjacency_matrix[i][j] = 0
             else:
                 break
 
-        if len(sens_in_circle) <= 3:
-            sample = len(sens_in_circle)
-        else:
-            sample = randint(2, len(sens_in_circle))
-        index_for_concat = choice(sens_in_circle, sample, p=prob_recalc(sens_in_circle))
-
-        for j in index_for_concat:
-            adjacency_matrix[j][i] = 1
-            adjacency_matrix[i][j] = 1
-        # выбираем сенсор для соединения и вносим информацию о связи в матрицу смежности
-        # index_for_concat = choice(in_circle[i],p=prob_recalc(in_circle[i]))
     return adjacency_matrix
 
 
@@ -70,8 +85,8 @@ if __name__ == '__main__':
             print('Вы ввели некоректное число, попробуйте снова!')
 
     adj = graph_generator(N)
-    for i in adj:
-        print(i)
+    for l in adj:
+        print(l)
 
     import networkx as nx
     import matplotlib.pyplot as plt
@@ -79,6 +94,3 @@ if __name__ == '__main__':
     g1 = nx.from_numpy_matrix(np.matrix(adj))
     nx.draw(g1, with_labels=True)
     plt.show()
-
-
-
