729cae999ee8c072a24ccbbc802c96aa22de0bba
Nickvi <Nickvi@bk.ru>
2017-11-12 16:17:47 +0300

    blocks refactored

--- main.py
+++ main.py
@@ -2,54 +2,6 @@
 import numpy as np
 import networkx as nx
 
-# Функция Виталия была временно закомментирована
-# def sensors_tree(adj_matrix):
-    # def dfs(node_index):
-    #     """
-    #     Обход дерева в глубину.
-    #     При обходе сенсор сразу пытается отдать сообщение
-    #     """
-    #     from_sensor = node_index
-    #     visited_sensors.append(from_sensor)
-    #     to_sensor = (adjacency_matrix[from_sensor].index(1)
-    #                  if adjacency_matrix[from_sensor].index(1) != from_sensor
-    #                  else adjacency_matrix[from_sensor].index(1, from_sensor + 1))
-    #     if (blocked_sensors[to_sensor] is not True  # если сенсор получатель не блокирован
-    #         and blocked_sensors[from_sensor] is not True  # и сенсор отправитель не блокирован
-    #         and need_send_msg[from_sensor]  # и сенсору необходимо послать сообщение:
-    #         and from_sensor != 0):
-    #         # определяем каким сенсорам необходимо блокировать передачу сообщений во избежания коллизий
-    #         indexes_for_block = indexes(adjacency_matrix[from_sensor], 1)
-    #         # добавляем в расписание сенсоры отправителя и получателя
-    #         schedule[slot_num].append((from_sensor, to_sensor))
-    #         # добавляем в очередь сообщений сенсора получателя(если не БС),
-    #         # если БС увеличиваем количество полученных сообщений на 1
-    #         if to_sensor != 0:
-    #             need_send_msg[to_sensor].append(from_sensor)
-    #         else:
-    #             need_send_msg[to_sensor] += 1
-    #         # блокируем сенсоры
-    #         for i in indexes_for_block:
-    #             blocked_sensors[i] = True
-    #         # удаляем сообщение из очереди сообщений сенсора отправителя
-    #         del need_send_msg[from_sensor][0]
-    #
-    #     for i, edge in enumerate(adjacency_matrix[from_sensor]):
-    #         if i not in visited_sensors and edge == 1:
-    #             dfs(i)
-    #
-    # adjacency_matrix = adj_matrix
-    # need_send_msg = [[i] if i != 0 else 0 for i in range(len(adjacency_matrix))]
-    # schedule = []
-    # slot_num = 0
-    # while need_send_msg[0] != len(adjacency_matrix) - 1:
-    #     blocked_sensors = [False for _ in range(len(adjacency_matrix))]
-    #     visited_sensors = []
-    #     schedule.append([])
-    #     dfs(0)
-    #     slot_num += 1
-    # return schedule
-
 
 def rasp_create(adj_matrix, balance=False):
     """
@@ -59,13 +11,13 @@ def rasp_create(adj_matrix, balance=False):
     :param balance: Бинарная опция включения/отключения балансировки
     :return: результат в формате расписания: [фрейм]
     """
-    result_way = []                             # Список передач за фрейм
-    trans_num = len(adj_matrix)                 # Число передатчиков
-    bs_buf = 0                                  # Количество сообщений на БС
+    result_way = []  # Список передач за фрейм
+    trans_num = len(adj_matrix)  # Число передатчиков
+    bs_buf = 0  # Количество сообщений на БС
     graph = nx.from_numpy_matrix(np.matrix(adj_matrix))
     if balance:
         routes_p_node = np.zeros((trans_num,), dtype=np.int)
-        trans_routes = [[] for _ in range(trans_num)]
+        trans_routes = [[]] * trans_num
         for i in range(trans_num):
             trans_routes[i] = nx.shortest_path(graph, 0, i)
             routes_p_node[trans_routes[i]] += 1
@@ -77,30 +29,31 @@ def rasp_create(adj_matrix, balance=False):
     else:
         trans_routes = nx.shortest_path(graph, 0)
 
-    while bs_buf < trans_num - 1:                       # Пока все заявки не попадут на БС,...
-        cur_transmission = []                           # Список передач за слот
-        trans_lock = [False for _ in range(trans_num)]  # Список заблокированных для передачи и приёма передатчиков
+    while bs_buf < trans_num - 1:  # Пока все заявки не попадут на БС,...
+        cur_transmission = []  # Список передач за слот
+        trans_lock = [False] * trans_num  # Список заблокированных для передачи передатчиков
+        receive_lock = [False] * trans_num  # Список заблокированных для приёма  передатчиков
 
         # В цикле исключена возможность передачи сообщения из БС (т.к. начинаем с 1)
         # Проходимся по сенсорам, проверяем возможность передачи и передаём
         for i in range(1, trans_num):
             # Проверка возможности передачи сообщения
             if len(trans_routes[i]) > 1:
-                source = trans_routes[i][-1]        # откуда передавать
-                receive = trans_routes[i][-2]       # куда передавать
+                source = trans_routes[i][-1]  # откуда передавать
+                receive = trans_routes[i][-2]  # куда передавать
 
                 # Проверка возможности передачи сообщения
                 trans_allowed = True
-                for j in range(trans_num):
-                    if adj_matrix[source][j] == 1 and trans_lock[j]:
-                        trans_allowed = False
+                if trans_lock[source] or receive_lock[receive]:
+                    trans_allowed = False
                 if trans_allowed:
                     # Добавление новой передачи в слот
                     cur_transmission.append([source, receive])
-                    # Блокировка ближайших передатчиков
-                    for j in range(trans_num):
-                        if adj_matrix[source][j] == 1:
-                            trans_lock[j] = True
+                    # Блокировка на передачу ближайших передатчиков
+                    for j, neighbor in enumerate(adj_matrix[source]):
+                        if neighbor == 1:
+                            receive_lock[j] = True
+                    trans_lock[receive] = True
                     trans_lock[source] = True
                     trans_routes[i].pop()
                     if len(trans_routes[i]) == 1:
@@ -125,7 +78,7 @@ def show_graph(graph):
 
 if __name__ == '__main__':
     from graph_gen import graph_generator
-    import validate     # Будет добавлено после тестирования корректности работы валидатора.
+    import validate  # Будет добавлено после тестирования корректности работы валидатора.
 
     # while True:
     #     try:
