bf228052457062c37eda09f9660b79ec0e02fdfc
Nickvi <Nickvi@bk.ru>
2017-12-01 02:02:01 +0300

    probability added

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import numpy as np
 import networkx as nx
+from random import random
+
+
+def message_come(prob):
+    return True if random() <= prob else False
 
 
 def rasp_create(adj_matrix, balance=False):
@@ -8,34 +13,38 @@ def rasp_create(adj_matrix, balance=False):
     связанной сети.
     :param adj_matrix: Матрица смежности. лист листов с описанием связей графового представления системы.
     :param balance: Бинарная опция включения/отключения балансировки
+    :param prob: Вероятность появления сообщения в слоте на каждом сенсоре
     :return: результат в формате расписания: [фрейм]
     """
     result_way = []  # Список передач за фрейм
-    trans_num = len(adj_matrix)  # Число передатчиков
+    sens_num = len(adj_matrix)  # Число передатчиков
     bs_buf = 0  # Количество сообщений на БС
     graph = nx.from_numpy_matrix(np.matrix(adj_matrix))
     if balance:
-        routes_p_node = np.zeros((trans_num,), dtype=np.int)
-        trans_routes = [[] for _ in range(trans_num)]
-        for i in range(trans_num):
+        routes_p_node = np.zeros((sens_num,), dtype=np.int)
+        trans_routes = [[] for _ in range(sens_num)]
+        for i in range(sens_num):
             trans_routes[i] = nx.shortest_path(graph, 0, i)
             routes_p_node[trans_routes[i]] += 1
             routes_p_node[0] = 0
             for j in trans_routes[i]:
                 for e_num in graph[j]:
-                    graph[j][e_num]['weight'] += routes_p_node[j] * trans_num ** -2
-                    graph[e_num][j]['weight'] += routes_p_node[j] * trans_num ** -2
+                    graph[j][e_num]['weight'] += routes_p_node[j] * sens_num ** -2
+                    graph[e_num][j]['weight'] += routes_p_node[j] * sens_num ** -2
     else:
         trans_routes = nx.shortest_path(graph, 0)
 
-    while bs_buf < trans_num - 1:  # Пока все заявки не попадут на БС,...
+    while bs_buf < sens_num - 1:  # Пока все заявки не попадут на БС,...
         cur_transmission = []  # Список передач за слот
-        trans_lock = [False] * trans_num  # Список заблокированных для передачи передатчиков
-        receive_lock = [False] * trans_num  # Список заблокированных для приёма  передатчиков
+        trans_lock = [False] * sens_num  # Список заблокированных для передачи передатчиков
+        receive_lock = [False] * sens_num  # Список заблокированных для приёма  передатчиков
 
         # В цикле исключена возможность передачи сообщения из БС (т.к. начинаем с 1)
         # Проходимся по сенсорам, проверяем возможность передачи и передаём
-        for i in range(1, trans_num):
+        for i in range(1, sens_num):
+
+
+
             # Проверка возможности передачи сообщения
             if len(trans_routes[i]) > 1:
                 source = trans_routes[i][-1]  # откуда передавать
@@ -64,6 +73,35 @@ def rasp_create(adj_matrix, balance=False):
     return result_way  # , len(result_way)                          # Вывод результата в формате [фрейм], число_слотов
 
 
+def sens_graph_with_prob(adj, sch, prb=None):
+    """
+    Моделирует буфер сенсоров в сенорной сети
+    :param adj: Матрица смежности сенсорной сети
+    :param sch: Расписание работы сенсорной сети
+    :param prb: Вероятность появления сообщения в кажом слоте для всех сенсоров
+    :return: среднее количество сообщений в буфере каждого сенсора
+    """
+    sensors_buffer = [1 if i != 0 else 0 for i in range(len(adj))]
+    for slot in sch:
+        # смотрим какие происходят передачи в слоте
+        # и изменяем количество сообщений в буфере
+        for transfer in slot:
+            sensors_buffer[transfer[0]] -= 1
+            if transfer[1] is not 0:
+                sensors_buffer[transfer[1]] += 1
+
+        # для всех сенсоров добавляем сообщение в буфер с вероятностью prob
+        for i in range(1, len(adj)):
+            if prb is not None:
+                assert type(prb) is float or prb == 1
+                if message_come(prb):
+                    sensors_buffer[i] += 1
+
+    # buff_mean = sum(sensors_buffer)/len(sensors_buffer[1:])
+    msg_count = sum(sensors_buffer)
+    return msg_count # buff_mean
+
+
 def show_graph(graph):
     """
     Функция для отображения графа
@@ -76,51 +114,3 @@ def show_graph(graph):
         graph = nx.from_numpy_matrix(np.matrix(graph))
     nx.draw_networkx(graph, with_labels=True)
     plt.show()
-
-
-if __name__ == '__main__':
-    from graph_gen import graph_generator, tree_generator, grid_generator
-    import validate  # Будет добавлено после тестирования корректности работы валидатора.
-
-    while True:
-        try:
-            method = int(input('''Выберите метод генерации
-            1 - дерево;
-            2 - решетка;
-            3 - случайный граф
-            
-            '''))
-            if method in [1, 2, 3]:
-                if method == 1 :
-                    N = int(input('Введите число сенсоров в сети: '))
-                    adjacency_matrix = tree_generator(N)
-                elif method == 3:
-                    N = int(input('Введите число сенсоров в сети: '))
-                    adjacency_matrix = graph_generator(N)
-                elif method == 2:
-                    N = int(input('Введите длину стороны решетки: '))
-                    if N % 2 == 0:
-                        raise ValueError
-                    else:
-                        adjacency_matrix = grid_generator(N)
-                break
-            else:
-                raise ValueError
-        except ValueError:
-            print('Вы ввели некоректное число, попробуйте снова!')
-
-    import time
-
-    start_time = time.time()
-    schedule = rasp_create(adjacency_matrix)
-    print('Is valid? {}'.format(validate.validateFunc(adjacency_matrix, schedule)))
-    print("--- %s seconds ---" % (time.time() - start_time))
-    print(schedule)
-    print('Длина расписания равна {}'.format(len(schedule)))
-    start_time = time.time()
-    schedule1 = rasp_create(adjacency_matrix, balance=True)
-    print('Is valid? {}'.format(validate.validateFunc(adjacency_matrix, schedule1)))
-    print("--- %s seconds ---" % (time.time() - start_time))
-    print(schedule1)
-    print('Длина расписания равна {}'.format(len(schedule1)))
-    show_graph(adjacency_matrix)
---
+++ prob_fig.py
@@ -0,0 +1,67 @@
+import main
+import plotly
+import plotly.graph_objs as go
+import validate
+
+from graph_gen import graph_generator, tree_generator, grid_generator
+
+while True:
+    try:
+        method = int(input('''Выберите метод генерации
+        1 - дерево;
+        2 - решетка;
+        3 - случайный граф
+
+        '''))
+        if method in [1, 2, 3]:
+            if method == 1:
+                N = int(input('Введите число сенсоров в сети: '))
+                adjacency_matrix = tree_generator(N)
+            elif method == 3:
+                N = int(input('Введите число сенсоров в сети: '))
+                adjacency_matrix = graph_generator(N)
+            elif method == 2:
+                N = int(input('Введите длину стороны решетки: '))
+                if N % 2 == 0:
+                    raise ValueError
+                else:
+                    adjacency_matrix = grid_generator(N)
+            break
+        else:
+            raise ValueError
+    except ValueError:
+        print('Вы ввели некоректное число, попробуйте снова!')
+
+schedule1 = main.rasp_create(adjacency_matrix, balance=True)
+
+buffer_mean = []
+num_of_exp = 100
+probabilities = [i * (1/num_of_exp) for i in range(1, num_of_exp)]
+frame_num = 1000
+
+for prob in probabilities:
+    acc = 0
+    for j in range(frame_num):
+        acc += main.sens_graph_with_prob(adjacency_matrix,
+                                         schedule1,
+                                         prb=prob)
+
+    buffer_mean.append(acc / frame_num)
+
+data = []
+trace1 = go.Scatter(
+    x=probabilities,
+    y=buffer_mean,
+    name='С балансировкой'
+ )
+data.append(trace1)
+
+
+layout = go.Layout(title=u"График зависимости количества сообщений в каждом сенсорном буфере от вероятности",
+                   xaxis=dict(title=u"Вероятность появления сообщения в сенсоре во время выполнения слота"),
+                   yaxis=dict(title=u"Среднее количество сообщений буфере")
+                   )
+
+plot = dict(data=data, layout=layout)
+
+plotly.offline.plot(plot, filename='prob.html')
