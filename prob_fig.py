8c9d9de6cc67c1a1d72313304a234703417928eb
Nickvi <nickvi@bk.ru>
2018-04-09 00:40:57 +0300

    mean request num and mean time were splited into different graphs

--- prob_fig.py
+++ prob_fig.py
@@ -2,105 +2,141 @@ import main
 import plotly
 import plotly.graph_objs as go
 import numpy as np
+import sys
 from interactive_console import interactive_console
 
 
-def draw_plot(title, x_title, y_title, **plot_data):
-    if "x_axis" not in plot_data or not data_for_plot["x_axis"]:
-        raise KeyError("Dictionary haven't '{0}' key or '{0}' have unexpected value".format("x_axis") )
+def draw_plot(title, x_title, y_title, file_name="plot.html", **plot_data):
+    if "x_axis" not in plot_data or not plot_data["x_axis"]:
+        raise KeyError("Dictionary haven't '{0}' key or '{0}' have unexpected value".format("x_axis"))
+    if "x_type" not in plot_data or not plot_data["x_type"]:
+        plot_data["x_type"] = "scatter"
+    if "y_type" not in plot_data or not plot_data["y_type"]:
+        plot_data["y_type"] = "scatter"
 
-    data = [
-            go.Scatter(x=plot_data["x_axis"], y=plot_data[name], name=name)
-            for name in plot_data if name != "x_axis"
-    ]
+    data = [go.Scatter(x=plot_data["x_axis"], y=plot_data[name], name=name)
+            for name in plot_data if name not in ["x_axis", "x_type", "y_type"]
+            ]
 
     layout = go.Layout(title=u"{}".format(title),
-                       xaxis=dict(title=u"".format(x_title)),
-                       yaxis=dict(title=u"".format(y_title)),)
+                       xaxis=dict(title=u"{}".format(x_title), type="{}".format(plot_data["x_type"])),
+                       yaxis=dict(title=u"{}".format(y_title), type="{}".format(plot_data["y_type"])),
+                       )
 
     plot = dict(data=data, layout=layout)
-    plotly.offline.plot(plot, filename='prob.html')
+    plotly.offline.plot(plot, filename=file_name)
 
 
-adjacency_matrix = interactive_console()
+if __name__ == "__main__":
+    adjacency_matrix = interactive_console()
 
-sensors_count = len(adjacency_matrix)-1
-frame = main.rasp_create(adjacency_matrix, balance=True)
-frame_len = len(frame)
+    sensors_count = len(adjacency_matrix)-1
+    frame = main.rasp_create(adjacency_matrix, balance=True)
+    frame_len = len(frame)
 
+    step = 1/sensors_count/10
+    probabilities = np.arange(0, 1 / frame_len, step, dtype=float)
+    probabilities = list(map(float, probabilities))
+    probabilities.append(0.124)
+    probabilities.append(0.126)
 
-step = 1/sensors_count/10
-probabilities = np.arange(0, 1 / frame_len, step, dtype=float)
-probabilities = list(map(float, probabilities))
-probabilities.append(0.124)
-probabilities.append(0.126)
+    teor_buff, buffer_mean, buff_adapt, n_avg_exp = [], [], dict(), []
+    mean_time, mean_time_adapt, mean_time_teor = [], dict(), []
 
+    buf = 0
+    num_of_frames = 1000
+    for i, prob in enumerate(probabilities):
 
-teor_buff, buffer_mean, buff_adapt, n_avg_exp = [], [], [], []
-mean_time = []
-mean_time_adapt = []
+        # стандартный режим алгоритма
+        buffer_mean.append(main.sens_graph_with_prob(adjacency_matrix, prb=prob, num_of_frames=num_of_frames))
+        sys.stdout.write("{:f.3}\r".format(prob))
+        # адаптивный режим алгоритма
+        for adapt_frame in range(1, 8):
 
-buf = 0
-num_of_frames = 1000
-for i, prob in enumerate(probabilities):
+            if adapt_frame not in buff_adapt:
+                buff_adapt[adapt_frame] = []
+            if adapt_frame not in mean_time_adapt:
+                mean_time_adapt[adapt_frame] = []
 
-    # стандартный режим алгоритма
-    buffer_mean.append(main.sens_graph_with_prob(adjacency_matrix, prb=prob, num_of_frames=num_of_frames))
+            buff_adapt[adapt_frame].append(main.sens_graph_with_prob(adjacency_matrix,
+                                                                       prb=prob,
+                                                                       num_of_frames=num_of_frames,
+                                                                       adaptation=adapt_frame))
+            try:
+                mean_time_adapt[adapt_frame].append((buff_adapt[adapt_frame][i] / (prob * sensors_count)))
+            except RuntimeWarning:
+                mean_time_adapt[adapt_frame].append(0)
+            except ZeroDivisionError:
+                mean_time_adapt[adapt_frame].append(0)
 
-    # адаптивный режим алгоритма
-    buff_adapt.append(main.sens_graph_with_prob(adjacency_matrix, prb=prob, num_of_frames=num_of_frames, adaptation=2))
-    print(prob)
-
-    # Теоретический расчет среднего количества сообщений в системе
-    lmd = frame_len * prob
-    q = 1-prob
-
-    mean_requests = (lmd*q+lmd*(1-lmd))/(2*(1-lmd))
-    teor_buff.append(mean_requests*sensors_count)
-
-    # "Работающий кодец"
-    avg_buf = 0
-    n_come = np.random.binomial(frame_len, prob, size=num_of_frames)
-    for cn in range(num_of_frames):
-        if buf == 0:
-            buf += n_come[cn]
-        else:
-            buf += n_come[cn]-1
-        avg_buf += buf
-    avg_buf /= num_of_frames
-    n_avg_exp.append(avg_buf*sensors_count)
-
-    try:
-        if buffer_mean:
-            mean_time.append(buffer_mean[i]/(prob*sensors_count))
-        if buff_adapt:
-            mean_time_adapt.append(buff_adapt[i]/(prob*sensors_count))
-    except RuntimeWarning:
-        continue
+            # print(adapt_frame/10)
 
 
-data_for_plot = dict(x_axis=probabilities)
+        # Теоретический расчет среднего количества сообщений в системе
+        lmd = frame_len * prob
+        q = 1-prob
 
-if buffer_mean:
-    data_for_plot['Практический результат'] = buffer_mean
+        mean_requests = (lmd*q+lmd*(1-lmd))/(2*(1-lmd))
+        teor_buff.append(mean_requests*sensors_count)
 
-if teor_buff:
-    data_for_plot['Теоретический график'] = teor_buff
+        # "Работающий кодец"
+        avg_buf = 0
+        n_come = np.random.binomial(frame_len, prob, size=num_of_frames)
+        for cn in range(num_of_frames):
+            if buf == 0:
+                buf += n_come[cn]
+            else:
+                buf += n_come[cn]-1
+            avg_buf += buf
+        avg_buf /= num_of_frames
+        n_avg_exp.append(avg_buf*sensors_count)
 
-if n_avg_exp:
-    data_for_plot['"Работающий кодец" :D'] = n_avg_exp
+        try:
+            if teor_buff:
+                mean_time_teor.append(teor_buff[i]/(prob*sensors_count))
+            if buffer_mean:
+                mean_time.append(buffer_mean[i]/(prob*sensors_count))
 
-if buff_adapt:
-    data_for_plot['Адаптированный'] = buff_adapt
+        except RuntimeWarning:
+            continue
+        except ZeroDivisionError:
+            continue
 
-if mean_time:
-    data_for_plot['Среднее время'] = mean_time
+    requests_for_plot = dict(x_axis=probabilities, y_type="log")
+    times_for_plot = requests_for_plot.copy()
 
-if mean_time_adapt:
-    data_for_plot['Среднее время адаптированное'] = mean_time_adapt
+    if buffer_mean:
+        requests_for_plot['Практический результат'] = buffer_mean
 
-draw_plot(title="Количество сообщений в системе от вероятности появления сообщения в сенсоре",
-          x_title="Вероятность появления сообщения в сенсоре во время выполнения слота",
-          y_title="Среднее количество сообщений в системе",
-          **data_for_plot)
+    if teor_buff:
+        requests_for_plot['Теоретический график'] = teor_buff
+
+    if n_avg_exp:
+        requests_for_plot['"Работающий кодец" :D'] = n_avg_exp
+
+    if buff_adapt:
+        for key in buff_adapt:
+            requests_for_plot["{}".format(key)] = buff_adapt[key]
+
+    if mean_time_teor:
+        times_for_plot['Среднее время теоретическое'] = mean_time_teor
+
+    if mean_time:
+        times_for_plot['Среднее время'] = mean_time
+
+    if mean_time_adapt:
+        for key in buff_adapt:
+            times_for_plot["{}".format(key)] = mean_time_adapt[key]
+
+    draw_plot(title="Количество сообщений в системе",
+              x_title="Вероятность появления сообщения в сенсоре",
+              y_title="Среднее количество сообщений в системе",
+              file_name="prob_fig.html",
+              **requests_for_plot)
+
+    draw_plot(title="Среднее время пребывания сообщения в системе",
+              x_title="Вероятность появления сообщения в сенсоре",
+              y_title="Время",
+              file_name="time_fig.html",
+              **times_for_plot)
 
